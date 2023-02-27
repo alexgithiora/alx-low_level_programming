79dba747d26711a7995def80fab32243bcb9e712
alexgithiora <alexgithiora@gmail.com>
2023-02-27 12:28:40 -0800

    change to rev str

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
-* rev_string - Reverses a string.
-* @s: string to be reserved.
-*/
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
 void print_rev(char *s)
 {
-int i = 0, len = 0;
-char tmp;
+	int len = 0, index;
 
-while (s[i++])
-len++;
+	while (s[index++])
+		len++;
 
-for (i = len - 1; i >= len / 2; i--)
-{
-tmp = s[i];
-s[i] = s[len - i - 1];
-s[len - i - 1] = tmp;
-}
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
-_putchar('\n');
+	_putchar('\n');
 }
