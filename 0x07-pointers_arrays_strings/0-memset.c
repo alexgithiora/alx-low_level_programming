31c727d9f58d3c70455e94f7d75d0eb5235f82db
alexgithiora <alexgithiora@gmail.com>
2023-03-07 03:53:51 -0800

    changed the crack code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
 *
 * Return: A pointer to the filled memory area @s.
 */
-void *_memset(void *s, int c, size_t n)
+void *_memset(char *s, char c, unsigned int n)
 {
-unsigned int index;
-unsigned char *memory = s, value = c;
-
-for (index = 0; index < n; index++)
-memory[index] = value;
-
-return (memory);
+unsigned int i;
+unsigned char *s = s;
+{
+for (i = 0; i < n; i++)
+s[i] = b;
+}
+return (s);
 }
